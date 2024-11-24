be7a2cca4b8464dddaacff73a6a249a863ad7d60
Michał Wojewoda <michal.wojewoda@hotmail.com>
2024-11-24 19:26:13 +0100

    Add WIP CreatePage.ps script

--- CreatePage.ps1
+++ CreatePage.ps1
@@ -1,17 +1,28 @@
+
+
 $htmlPath = ".\hello.html"
 $targetFile = Get-ChildItem -Path $htmlPath
 $zipFilePath = $targetFile.BaseName + ".zip"
 Compress-Archive -Path $htmlPath -DestinationPath $zipFilePath
 
-$webapp = Get-AzWebApp -Name $appName -ResourceGroupName $resourceGroupName
-$kuduUrl = "https://$($webapp.DefaultHostName)/api/zipdeploy"
-$publishingProfile = (Get-AzWebAppPublishingProfile -ResourceGroupName $resourceGroupName -WebAppName $appName)
-$headers = @{
-    Authorization = "Basic $([Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($publishingProfile.UserName):$($publishingProfile.Password)")))"
+$ResourceGroupName = Get-AzResourceGroup | Where-Object {$_.Tags.'x-module-id' -eq "learn.azure.deploy-azure-resources-bicep"} | Select-Object -ExpandProperty ResourceGroupName
+
+$webapp = Get-AzWebApp -ResourceGroupName $ResourceGroupName
+$publishingProfile = Get-AzWebAppPublishingProfile -WebApp $webapp
+
+$invokeWebRequestParameters = @{
+    Uri = "https://$($webapp.DefaultHostName)/api/zipdeploy"
+    Headers = @{
+        Authorization = "Basic $([Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($publishingProfile.UserName):$($publishingProfile.Password)")))"
+    }
+    InFile = $zipFilePath
+    Method = "POST"
 }
-Invoke-WebRequest -Uri $kuduUrl -Headers $headers -InFile "./site.zip" -Method POST
+
+$invokeWebRequestParameters
+Invoke-WebRequest @invokeWebRequestParameters
 
 
 
 #Final Cleanup
-Remove-Item $zipFilePath
+#Remove-Item $zipFilePath
